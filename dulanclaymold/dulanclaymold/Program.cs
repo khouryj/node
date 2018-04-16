f638bb021e77f53e7e4abe394bebf62ed9a3e246
git outta here <nou@fasteddie.com>
2018-04-16 11:12:57 -0400

    klu klux krab

---
+++ dulanclaymold/dulanclaymold/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dulanclaymold
+{
+    public enum TraverseOrder
+    {
+        InOrder,
+        PreOrder,
+        PostOrder
+    }
+
+    public class ADSTree
+    {
+        private ADSNode root;
+
+        public sealed class ADSNode
+        {
+            public ADSNode left;
+            public ADSNode right;
+            public int key;
+            public int cardinality;  //  Increment each time duplicates are added
+            public int height; // Height of this node
+
+            public ADSNode(int val)
+            {
+                key = val;
+            }
+        }
+
+        public ADSTree()
+        {
+            root = null;
+        }
+
+        // Return the node where value is located
+        public ADSNode find(int value)
+        {
+            if (value == root.key)
+            {
+                return root;
+            }
+            ADSNode current = root;
+            while (value != current.key)
+            {
+                if (value < current.key)
+                {
+                    current = current.left;
+                }
+                else
+                {
+                    current = current.right;
+                }
+            }
+            if (value != current.key)
+            {
+                return null;
+            }
+            return current;
+        }
+
+        // Inserts a node into the tree and maintains it's balance
+        public void insert(int value)
+        {
+            if (root == null)
+            {
+                root = new ADSNode(value);
+                root.height = 0;
+            }
+        }
+
+        // Print the tree in a particular order
+        public void printTree(TraverseOrder order)
+        {
+        }
+    }
+}
---
+++ dulanclaymold/dulanclaymold/seven.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dulanclaymold
+{
+    class seven
+    {
+        static void Main()
+        {
+            ADSTree t = new ADSTree();
+
+            t.insert(43);
+            t.insert(18);
+            t.insert(22);
+            t.insert(9);
+            t.insert(21);
+            t.insert(6);
+            t.insert(8);
+            t.insert(20);
+            t.insert(63);
+            t.insert(50);
+
+            t.printTree(TraverseOrder.InOrder);
+        }
+    }
+}
